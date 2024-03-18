c4eb56928333e76226c84fc8a39adab91efb8c38
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-18 18:53:44 +0300

    commit

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,28 @@
-const infoBtns = document.querySelectorAll(".info-dot")
-const infoHints = document.querySelectorAll(".info-hint")
-
+const infoBtns = document.querySelectorAll(".info-dot");
+const infoHints = document.querySelectorAll(".info-hint");
 
 //клік на btn з підсказкою
 for (let btn of infoBtns) {
-    btn.addEventListener('click', showHint);
+  btn.addEventListener("click", showHint);
 }
 
-function showHint () {
-    this.parentNode.querySelector('.info-hint').classList.toggle('none')
+function showHint(e) {
+  e.stopPropagation();
+  this.parentNode.querySelector(".info-hint").classList.toggle("none");
 }
 
 //закриваємо підсказку при кліку при всій сторінці
 
-document.addEventListener('click', closeHints)
+document.addEventListener("click", closeHints);
 
-function closeHints () {
-    for (let hint of infoHints ) {
-        hint.classList.add('none')
-    }
+function closeHints() {
+  for (let hint of infoHints) {
+    hint.classList.add("none");
+  }
+}
+
+// клік на підсказкі
+
+for (let hint of infoHints) {
+  hint.addEventListener("click", (e) => e.stopPropagation());
 }
