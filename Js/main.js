ec3b55b31c8a72482aa8da349d47526e7db50756
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-19 14:09:06 +0300

    desk-swiper

--- Js/main.js
+++ Js/main.js
@@ -36,6 +36,22 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 4,
   spaceBetween: 42,
+  freeMode: true,
+
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   },
+  // },
 
   // Navigation arrows
   navigation: {
