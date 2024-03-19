0ffc45e26e0f0a88ceeeba95c20b4fb98fc66187
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-19 13:53:29 +0300

    swiper

--- Js/main.js
+++ Js/main.js
@@ -26,3 +26,28 @@ function closeHints() {
 for (let hint of infoHints) {
   hint.addEventListener("click", (e) => e.stopPropagation());
 }
+
+
+//<------------------------------------------------------------   Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
