ceacf93605ecf108d4cc03e263752228d86663c2
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-23 17:22:02 +0300

    mobile

--- Js/main.js
+++ Js/main.js
@@ -34,24 +34,28 @@ for (let hint of infoHints) {
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 42,
   freeMode: true,
 
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40,
-  //   },
-  //   1024: {
-  //     slidesPerView: 5,
-  //     spaceBetween: 50,
-  //   },
-  // },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 22,
+    },
+    1230: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 42,
+    },
+  },
 
   // Navigation arrows
   navigation: {
